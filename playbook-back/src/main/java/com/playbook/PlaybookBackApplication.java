d1871c7ecdf4fb5bdb90a4d8e4ace9288d01f096
José González Sánchez <jose.gonzalez@kairosds.com>
2018-09-24 17:11:47 +0200

    Primer commit backend

---
+++ playbook-back/src/main/java/com/playbook/PlaybookBackApplication.java
@@ -0,0 +1,12 @@
+package com.playbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaybookBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaybookBackApplication.class, args);
+	}
+}
