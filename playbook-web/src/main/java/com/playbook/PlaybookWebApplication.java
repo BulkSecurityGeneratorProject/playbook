a4aa3b689ee7a2e63cb9a70927d9c13b26cb1775
José González Sánchez <jose.gonzalez@kairosds.com>
2018-09-24 12:32:09 +0200

    Primer commit bien generado

---
+++ playbook-web/src/main/java/com/playbook/PlaybookWebApplication.java
@@ -0,0 +1,12 @@
+package com.playbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaybookWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaybookWebApplication.class, args);
+	}
+}
---
+++ playbook-web/src/test/java/com/playbook/PlaybookWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.playbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlaybookWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
